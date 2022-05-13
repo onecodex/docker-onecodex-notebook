2b67f5faab841e4f14adce9d3d6f9e230d22a53c
Paweł Konieczny <pawel.konieczny@invitae.com>
2022-05-13 12:21:30 +0200

    Fix dependency issues. Add XSRF config

--- notebook/jupyter_notebook_config.py
+++ notebook/jupyter_notebook_config.py
@@ -11,6 +11,7 @@ c = get_config()
 c.NotebookApp.ip = '0.0.0.0'
 c.NotebookApp.port = 8888
 c.NotebookApp.open_browser = False
+c.NotebookApp.tornado_settings = {"xsrf_cookies": False}
 
 # Prefer JavaScript over SVG when exporting notebooks as HTML--keeps figures interactive
 c.NbConvertBase.display_data_priority = [
